08f3753c1b1b11fb0def69cd2b9b858fd15d5fcb
unknown <saba.tehrani.eng@gmail.comgit config --global user.name uvsq21802692git config --global user.email saba.tehrani.eng@gmail.com>
2019-02-12 15:30:31 +0330

    first program

---
+++ src/sum.kt
@@ -0,0 +1,11 @@
+class sum {
+
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun main(args : Array<String>) {
+        println("sum of the numbers : "+ sum(2,5))
+    }
+}
